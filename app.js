82b0e1ced64fa384b6f71d680598bd2d5f04588c
chuberacroi412 <lamanhtuan1299@gmail.com>
2022-05-05 14:11:35 +0700

    blockchain

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ const userService = require('./services/user')
 
 // static
 const PORT = 3001;
-let blockChain = []
+let blockChain = {
+    chain: [],
+    difficulty: 1,
+    blockTime: 30000
+}
+
+const server = {
+    wallet : userService.CreateWallet(),
+    giftCoin: 10
+}
 
 // app
 const app = express()
@@ -26,7 +35,41 @@ app.listen(PORT, () => console.log('on'))
 app.get('/user/wallet', (req, res) => {
 
     const result = userService.CreateWallet()
+    userService.RegisterGift(blockChain, server, result.publicKey)
+
     res.status(200).send(result)
+})
+
+app.get('/user/balance/:id', (req, res) => {
+    
+    const balance = userService.GetAccountBalance(blockChain.chain, req.params.id)
+    res.status(200).send(balance.toString())
+})
+
+app.get('/user/history/:id', (req, res) => {
+
+    const history = userService.GetTransactionHistory(blockChain.chain, req.params.id)
+    res.status(200).send(history)
+})
+
+app.post('/user/wallet', (req, res) => {
+
+    if(req.body.userPublicKey) {
+       const result = userService.IsKeyExisted(blockChain.chain, req.body.userPublicKey)
+       res.status(200).send(result)
+    }
+    else
+        res.status(400).send('missing wallet address')
+})
+
+app.get('/demo', (req, res) => {
+
+    const data = {
+        'name' : 'hehehe',
+        'age': 12
+    }
+    userService.AddBlock(blockChain, data)
+    res.status(200).send(blockChain.chain)
 })
 
 app.get('*', (req,res) => {
--- services/user.js
+++ services/user.js
@@ -1,5 +1,6 @@
 const EC = require('elliptic').ec
 const ec = new EC('secp256k1')
+const crypto = require('crypto')
 
 const CreateWallet = function() {
     
@@ -13,4 +14,120 @@ const CreateWallet = function() {
     }
 }
 
-module.exports = {CreateWallet}
+const GetAccountBalance = function(chain, userPublicKey) {
+    let balance = 0
+
+    if(chain) {
+        chain.forEach(block => {
+            if(block.data.to == userPublicKey) {
+                balance += +block.data.amount
+            }
+        });
+    }
+    
+
+    return balance
+}
+
+const GetTransactionHistory = function(chain, userPublicKey) {
+
+    let history = []
+
+    if(chain) {
+        chain.forEach(block => {
+            if(block.data.from === userPublicKey || block.data.to === userPublicKey) {
+                history.push(block.data)
+            }
+        })
+    }
+    
+
+    return history
+}
+
+const IsKeyExisted = function(chain, userPublicKey) {
+
+    if(chain) {
+        chain.forEach(block => {
+            if(block.data.from === userPublicKey || block.data.to === userPublicKey)
+                return true
+        })
+    }
+
+    return false
+}
+
+function SHA256(input) {
+    return crypto.createHash('sha256').update(input).digest('hex')
+}
+
+function HashData(data, timeStamp, prevHash, nonce) {
+    return SHA256(JSON.stringify(data) + timeStamp + prevHash + nonce)
+}
+
+function mine(difficulty, data, timeStamp, prevHash) {
+    let nonce = 0
+    let hash = HashData(data, timeStamp, prevHash, nonce)
+
+    while(!hash.startsWith(Array(difficulty + 1).join('0'))) {
+        nonce++;
+        hash = HashData(data, timeStamp, prevHash, nonce)
+    }
+
+    return {
+        nonce, hash
+    }
+}
+
+function CreateBlock(difficulty, timeStamp, data, prevHash) {
+
+    const mineResult = mine(difficulty, data, timeStamp, prevHash)
+
+    return {
+        timeStamp: timeStamp,
+        data: data,
+        hash: mineResult.hash,
+        prevHash: prevHash,
+        nonce: mineResult.nonce
+    }
+}
+
+function GetLastBlock(chain) {
+    
+    if(chain && chain.length > 0) {
+        return chain[chain.length - 1]
+    }
+
+    return null
+}
+
+const AddBlock = function (blockChain, data) {
+    const lastBlock = GetLastBlock(blockChain.chain)
+    let prevHash = null
+
+    if(lastBlock)
+        prevHash = lastBlock.hash
+    
+    let newBlock = CreateBlock(blockChain.difficulty, Date.now().toString(), data, prevHash)
+
+    blockChain.chain.push(newBlock)
+
+    if(blockChain.difficulty >= 5) {
+        blockChain.difficulty = 1
+    }
+    else if(lastBlock) {
+        blockChain.difficulty += Date.now() - parseInt(GetLastBlock(blockChain.chain).timeStamp) < blockChain.blockTime ? 1 : -1
+    }
+}
+
+const RegisterGift = function(blockChain, server, userPublicKey) {
+    let data = {
+        from: server.wallet.publicKey,
+        to: userPublicKey,
+        amount: 10
+    }
+
+    AddBlock(blockChain, data)
+}
+
+module.exports = {CreateWallet, GetAccountBalance, GetTransactionHistory, IsKeyExisted, AddBlock, RegisterGift}
